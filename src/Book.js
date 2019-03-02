56da8d6c515565903416ddfe2397dc53b74af27b
Prashanth <yvprashanth@gmail.com>
2019-03-01 22:49:10 -0800

    UI Shows up fine

--- src/Book.js
+++ src/Book.js
@@ -7,12 +7,12 @@ class Book extends React.Component {
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{width:128, height:192, backgroundImage: `url(${this.props.bookDetails.imageLinks.thumbnail})`}}>
+                    <div className="book-cover" style={{width:128, height:193, backgroundImage: `url(${this.props.bookDetails.imageLinks.thumbnail})`}}>
                        <ShelfChanger />
                     </div>
-                    <div className="book-title">{this.props.bookDetails.title}</div>
-                    <div className="book-authors">{this.props.bookDetails.authors[0]}</div>
                 </div>
+                <div className="book-title">{this.props.bookDetails.title}</div>
+                <div className="book-authors">{this.props.bookDetails.authors[0]}</div>
             </div>
         )
     }
