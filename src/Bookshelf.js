dc1ab82e850ff5f4b294cb641b76b057a1751c2d
Prashanth <yvprashanth@gmail.com>
2019-03-01 21:30:37 -0800

    Some more

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -6,7 +6,18 @@ class Bookshelf extends React.Component {
     render(){
         return (
             <div className="bookshelf">
-
+                <h2 className="bookshelf-title"></h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {this.props.books.map((book) => {
+                            return (
+                                <li>
+                                    <Book bookDetails={book} />
+                                </li>
+                            )
+                        })}
+                    </ol>
+                </div>
             </div>
         )
     }
