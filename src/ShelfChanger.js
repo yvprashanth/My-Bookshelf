0d13d2b2944908ad9f0b20edb210d4660afe6e70
Prashanth <yvprashanth@gmail.com>
2019-03-01 21:32:25 -0800

    Slightly more

---
+++ src/ShelfChanger.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+class ShelfChanger extends React.Component {
+    render(){
+        return (
+            <div className="book-shelf-changer">
+                <select>
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="currentlyReading">Want to Read</option>
+                    <option value="currentlyReading">Read</option>
+                    <option value="currentlyReading">None</option>
+                </select>
+            </div>
+        )
+    }
+}
