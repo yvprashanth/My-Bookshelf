ce1b8e0869e754a4ce670fcd6687428ede5dbf5e
Prashanth <yvprashanth@gmail.com>
2019-03-10 12:47:20 -0700

    More small changes

---
+++ build/precache-manifest.e97023d105e8c72818baf45d5b12c0eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "fe6fffbb939330a5ac80",
+    "url": "/static/js/main.fe6fffbb.chunk.js"
+  },
+  {
+    "revision": "59b883956b7f589c5bf6",
+    "url": "/static/js/2.59b88395.chunk.js"
+  },
+  {
+    "revision": "fe6fffbb939330a5ac80",
+    "url": "/static/css/main.05047358.chunk.css"
+  },
+  {
+    "revision": "78788eb1b09d003bcdf08790214f5ca4",
+    "url": "/index.html"
+  }
+];
